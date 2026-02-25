3aa6d266cd7976ec4ba6fb6c74baee7038e86df2
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-25 15:13:57 +0530

    update routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,41 @@
+import BlogEditor from './components/editor';
 import App from './App';
+import IsAuthenticated from './components/authenticate';
+import ErrorPage from './components/error/serverError';
+import SigninForm from './components/login';
+import { Profile } from './components/profile';
 
 const routes = [
     {
         path: '/',
-        Component: App
+        Component: App,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                Component: IsAuthenticated,
+                errorElement: <ErrorPage />,
+                children: [
+                    {
+                        index: true,
+                        Component: Profile,
+                    }
+                ]
+            },
+            {
+                path: '/Sign-in',
+                Component: SigninForm,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: '/posts/:postid',
+                Component: BlogEditor
+            },
+            {
+                path: '/create',
+                Component: BlogEditor
+            }
+        ]
     }
 ]
 
